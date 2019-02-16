65e43c53c3052729033a9f1921740da103db5699
Keanu Interone <keanu.interone@gmail.com>
2019-02-15 21:49:43 -0700

    added API for guns

---
+++ app/controllers/api/guns_controller.rb
@@ -0,0 +1,53 @@
+class Api::GunsController < ApplicationController
+  protect_from_forgery with: :null_session
+  before_action :set_gun, only: [:show, :update, :destroy]
+
+  # GET /api/guns
+  def index
+    @guns = Gun.all
+
+    render json: @guns
+  end
+
+  # GET /api/guns/1
+  def show
+    render json: @gun
+  end
+
+  # POST /api/guns
+  def create
+    @gun = Gun.new(gun_params)
+
+    if @gun.save
+      render json: @gun, status: :created, location: @gun
+    else
+      render json: @gun.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /api/guns/1
+  def update
+    if @gun.update(gun_params)
+      render json: @gun
+    else
+      render json: @gun.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /api/guns/1
+  def destroy
+    @gun.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_gun
+      @gun = Gun.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def gun_params
+      params.require(:gun).permit(:name, :caliber, :brand_id)
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  resources :brands
+  
   root 'guns#index'
+  resources :brands
   resources :guns
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api, :defaults => {:format => :json} do
+  	resources :guns
+  end
 end
